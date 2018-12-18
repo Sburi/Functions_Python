aa6259e62d9578781ebe5219657d686833c24f3e
Sburi <41414125+Sburi@users.noreply.github.com>
2018-12-17 19:36:20 -0600

    Adding MinValue.

---
+++ f_MinValue.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 23:37:09 2018
+
+@author: steve
+"""
+#Prompt: Find the smallest number in a list
+#MyCode
+#def find_smallest_int(arr):
+#    arr.sort()
+#    return arr[0]
+
+#BestCode
+def findSmallestInt(arr):
+    return min(arr)
